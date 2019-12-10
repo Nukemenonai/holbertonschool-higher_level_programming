fb5b49ed0b8d27b0a8ecc90e8d6192aebad6664b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-10 14:45:46 -0500

    changed number of iterations

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
+    for i in range(1, 100):
         if (i % 3 == 0) and (i % 5 == 0):
             print("FizBuzz", end=" ")
         else:
@@ -10,3 +10,4 @@ def fizzbuzz():
                 print("Buzz", end=" ")
             else:
                 print("{}".format(i), end=" ")
+    print("Buzz", end=" ")
