e78105fc5b18a7db18f12e9fb167f8e5fd38a014
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-10 14:51:10 -0500

    fixed unneeded else

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,10 @@ def fizzbuzz():
     for i in range(1, 100):
         if (i % 3 == 0) and (i % 5 == 0):
             print("FizBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
         else:
-            if i % 3 == 0:
-                print("Fizz", end=" ")
-            elif i % 5 == 0:
-                print("Buzz", end=" ")
-            else:
-                print("{}".format(i), end=" ")
+            print("{}".format(i), end=" ")
     print("Buzz", end=" ")
