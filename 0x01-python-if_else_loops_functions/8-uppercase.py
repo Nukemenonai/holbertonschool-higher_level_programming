9345d11793478f11526ad3ae4b70b99618dfa865
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-10 12:55:42 -0500

    [ADD]: task 8 - uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    l = len(str)
+    for i in range(0, l):
+        char = str[i]
+        if i != (len(str) - 1):
+            if ord(char) >= 97 and ord(char) <= 122:
+                char = chr(ord(char) - 32)
+            print(char, end="")
+        else:
+            if ord(char) >= 97 and ord(char) <= 122:
+                char = chr(ord(char) - 32)
+            print(char)
