af7ca474214f84ba03d863e389b32fe1043aba18
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-10 14:57:40 -0500

    fix removed parenthesis

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 100):
-        if (i % 3 == 0) and (i % 5 == 0):
+        if i % 3 == 0 and i % 5 == 0:
             print("FizBuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
