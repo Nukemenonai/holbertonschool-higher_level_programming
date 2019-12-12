da3c82364f0527ca12d15f737b4dd57fd4c556a8
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-12 17:27:40 +0000

    fix: added semicolon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv)
     print("{} arguments".format(length - 1) if length > 2
-          else "{} argument".format(length - 1))
+          else "{} argument:".format(length - 1))
     for i in range(1, length):
         print("{}: {}".format((i), sys.argv[i]))
