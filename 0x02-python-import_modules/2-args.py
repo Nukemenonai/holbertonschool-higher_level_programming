fb03b0909b91583ea51bbeb7d15228977467cd8c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-12 17:25:18 +0000

    [fix]: added cond for only 1 arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import sys
     length = len(sys.argv)
-    print("{} arguments".format(length - 1))
+    print("{} arguments".format(length - 1) if length > 2
+          else "{} argument".format(length - 1))
     for i in range(1, length):
         print("{}: {}".format((i), sys.argv[i]))
