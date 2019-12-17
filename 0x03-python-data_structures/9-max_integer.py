71c519d568e7bbac4ab6846eea0939929578aad7
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 16:07:27 -0500

    fix: adding case for no list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_num = 0
+    if not my_list:
+        return max_num
     for number in my_list:
         if number > max_num:
             max_num = number
