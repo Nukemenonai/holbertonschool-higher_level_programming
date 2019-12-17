1e0659ac3a6ceadf84c2ed0d0b03f6ec97a9de50
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 14:36:25 -0500

    fix: added aditional logic for void subarrays

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print()
-    if len(matrix) == 0:
-        print()
-    else:
+    if matrix:
         for i in range(0, len(matrix)):
             for j in range(0, len(matrix[i])):
                 if j < len(matrix[i]) - 1:
                     print("{:d}".format(matrix[i][j]), end=" ")
                 else:
                     print("{:d}".format(matrix[i][j]))
+            if not matrix[i]:
+                print()
