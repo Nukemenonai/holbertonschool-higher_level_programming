85f2f7a7f2a7a3bd9e6725a0a7699dc177e7a8ef
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 17:06:21 -0500

    fix : third test

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,14 +4,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     if not tuple_b:
         tuple_b = (0, 0)
-    elif len(tuple_a) > 2:
-        tuple_a = tuple_a[0:2]
-    elif len(tuple_b) > 2:
-        tuple_b = tuple_b[0:2]
-    elif len(tuple_a) >= 0 and len(tuple_a) < 2:
+    if len(tuple_a) >= 0 and len(tuple_a) < 2:
         tuple_a = (tuple_a[0], 0)
-    elif len(tuple_b) >= 0 and len(tuple_b) < 2:
+    if len(tuple_b) >= 0 and len(tuple_b) < 2:
         tuple_b = (tuple_b[0], 0)
+    if len(tuple_a) > 2:
+        tuple_a = tuple_a[0], tuple_a[1]
+    if len(tuple_b) > 2:
+        tuple_b = tuple_b[0], tuple_b[1]
     el_one = tuple_a[0] + tuple_b[0]
     el_two = tuple_a[1] + tuple_b[1]
     new_tup = (el_one, el_two)
