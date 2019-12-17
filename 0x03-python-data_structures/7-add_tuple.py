6d508b7c8975ed91d67edf346c30047806a9b08c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 16:43:20 -0500

    fix: defualt case

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if not tuple_a:
-        tuple_a = (0, 0)
-    if not tuple_b:
-        tuple_b = (0, 0)
-    elif tuple_a == ():
+    new_tup = (0, 0)
+    if tuple_a == ():
         tuple_a = (0, 0)
     elif tuple_b == ():
         tuple_b = (0, 0)
