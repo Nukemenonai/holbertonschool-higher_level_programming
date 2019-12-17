b5e6842015faa8ee913cc84fa6c80e4a82649e3b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 10:21:49 -0500

    [ADD]: task 4 - replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cplist = my_list[:]
+    if idx < 0 or idx > len(my_list):
+        return cplist
+    else:
+        cplist[idx] = element
+        return cplist
