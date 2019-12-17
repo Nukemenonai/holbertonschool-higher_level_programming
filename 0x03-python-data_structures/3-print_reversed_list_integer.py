a7e72208a4606f2327bbeb6148bbe4b198bc6ee7
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 13:45:07 -0500

    fix: error in syntax

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        for i in my_list[::-1]
+        for i in my_list[::-1]:
             print("{:d}".format(i))
