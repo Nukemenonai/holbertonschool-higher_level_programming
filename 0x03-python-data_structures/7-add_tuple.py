c132d57e8640868c45003a72cbd7ee599327cfb9
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 17:03:10 -0500

    fix : third test

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tup = (0, 0)
     if not tuple_a:
         tuple_a = (0, 0)
     if not tuple_b:
-        tuple_b = (0, 0)
-    if tuple_a == ():
-        tuple_a = (0, 0)
-    elif tuple_b == ():
         tuple_b = (0, 0)
     elif len(tuple_a) > 2:
         tuple_a = tuple_a[0:2]
