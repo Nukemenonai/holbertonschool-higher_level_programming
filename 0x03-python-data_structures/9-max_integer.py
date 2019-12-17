3578a837e725159937f4aa1581ede588a9a0c753
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 16:08:39 -0500

    fix: return for no list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     max_num = 0
     if not my_list:
-        return max_num
+        return None
     for number in my_list:
         if number > max_num:
             max_num = number
