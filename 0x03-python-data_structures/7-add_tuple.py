71438a7e78297bc4d52c962f529e34aaecef3995
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-17 15:06:03 -0500

    fix: what happens in void param case

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a == ():
+    if not tuple_a:
+        tuple_a = (0, 0)
+    if not tuple_b:
+        tuple_b = (0, 0)
+    elif tuple_a == ():
         tuple_a = (0, 0)
     elif tuple_b == ():
         tuple_b = (0, 0)
