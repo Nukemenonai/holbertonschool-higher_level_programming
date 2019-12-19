a06bf4205c4f27c4cd2492248252bb1d76e043c4
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 14:56:03 -0500

    fix: bad return

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,6 +9,6 @@ def best_score(a_dictionary):
             if a_dictionary[key] > best_score:
                 best_score = a_dictionary[key]
                 best_key = key
-                return key
             elif key not in a_dictionary:
                 return None
+    return best_key
