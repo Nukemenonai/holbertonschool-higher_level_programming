e177072c325f18b4995553555325a428289c48f7
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 14:51:25 -0500

    [ADD]: task 10, best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        best_score = 0
+        best_key = ""
+        for key in a_dictionary:
+            if a_dictionary[key] > best_score:
+                best_score = a_dictionary[key]
+                best_key = key
+                return key
+            elif key not in a_dictionary:
+                return None
