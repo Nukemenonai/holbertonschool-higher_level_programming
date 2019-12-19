7d01cb492447d03e7ee903a92d3130bf762da06a
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 13:25:24 -0500

    [fix] use of list comprehension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for item in new_list:
-        if item == search:
-            new_list[item] = replace
+    new_list = [replace if item == search else item for item in my_list]
     return new_list
