44e39cac3f1efcb3d715b7cabea5c3061b5ab1eb
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 18:48:46 -0500

    [ADD]: adv task 1 weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    avg = 0
+    dvdr = 0
+    if not my_list:
+        return 0
+    else:
+        for i in my_list:
+            avg += (i[0]*i[1])
+            dvdr +=(i[1])
+        return avg / dvdr
