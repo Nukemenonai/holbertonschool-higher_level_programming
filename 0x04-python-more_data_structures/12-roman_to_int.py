b35fa667681e6593a4e6e0111d82a9256439e4b1
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 17:07:53 -0500

    fix: bad repetition count system

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     curr = 0
     lttr = roman_string[curr]
     total = 0
-    rep_cnt = 1
+    ocurr = 1
     if not roman_string:
         return 0
     else:
@@ -17,15 +17,17 @@ def roman_to_int(roman_string):
                     curr += 1
                 else:
                     if romans[lttr] < romans[roman_string[curr - 1]]:
+                        ocurr = 1
                         total = romans[lttr] + total
                         curr += 1
                     elif romans[lttr] > romans[roman_string[curr - 1]]:
+                        ocurr = 1
                         total = romans[lttr] - total
                         curr += 1
                     elif romans[lttr] == romans[roman_string[curr - 1]]:
                         total = romans[lttr] + total
                         curr += 1
-                        rep_cnt += 1
-                        if rep_cnt == 4:
+                        ocurr += 1
+                        if ocurr >= 4:
                             return 0
         return total
