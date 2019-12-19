82235c72b53b64231841dea2c9f7fa778e7ad379
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 18:50:40 -0500

    fix: pep format

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -7,5 +7,5 @@ def weight_average(my_list=[]):
     else:
         for i in my_list:
             avg += (i[0]*i[1])
-            dvdr +=(i[1])
+            dvdr += (i[1])
         return avg / dvdr
