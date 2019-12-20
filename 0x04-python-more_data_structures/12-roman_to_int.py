e618242f16d57ee20bfe07c56e1f58b6652390cd
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 20:37:54 -0500

    fix: order of if and else clauses

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str:
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    if roman_string is None:
-        return 0
-    if len(roman_string) == 0:
-        return 0
+    #if len(roman_string) == 0:
+        #return 0
+
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     curr = 0
     lttr = roman_string[curr]
     total = 0
     ocurr = 1
-    
+
     for lttr in roman_string:
         if lttr not in romans:
             return 0
+        if curr == 0:
+            total = romans[lttr]
+            curr += 1
         else:
-            if curr == 0:
-                total = romans[lttr]
+            if romans[lttr] < romans[roman_string[curr - 1]]:
+                ocurr = 1
+                total = romans[lttr] + total
                 curr += 1
-            else:
-                if romans[lttr] < romans[roman_string[curr - 1]]:
-                    ocurr = 1
-                    total = romans[lttr] + total
-                    curr += 1
-                elif romans[lttr] > romans[roman_string[curr - 1]]:
-                    ocurr = 1
-                    total += romans[lttr]
-                    total -= (2 * romans[roman_string[curr - 1]])
-                    curr += 1
-                elif romans[lttr] == romans[roman_string[curr - 1]]:
-                    total = romans[lttr] + total
-                    curr += 1
-                    ocurr += 1
-                    if ocurr >= 4:
-                        return 0
-            return total
+            elif romans[lttr] > romans[roman_string[curr - 1]]:
+                ocurr = 1
+                total += romans[lttr]
+                total -= (2 * romans[roman_string[curr - 1]])
+                curr += 1
+            elif romans[lttr] == romans[roman_string[curr - 1]]:
+                total = romans[lttr] + total
+                curr += 1
+                ocurr += 1
+                if ocurr >= 4:
+                    return 0
+    return total
