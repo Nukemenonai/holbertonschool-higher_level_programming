93d60b086c268826dd1fc0df6a3e237c2bd2e6de
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 19:58:00 -0500

    fix: removed logic of compound integers, moved to a simpler logic

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romans = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50,
-              'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'MC': 900, 'M': 1000}
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     curr = 0
     lttr = roman_string[curr]
     total = 0
@@ -25,16 +24,9 @@ def roman_to_int(roman_string):
                         curr += 1
                     elif romans[lttr] > romans[roman_string[curr - 1]]:
                         ocurr = 1
-                        compound = roman_string[curr - 1] + roman_string[curr]
-                        if compound in romans:
-                            total -= romans[roman_string[curr - 1]]
-                            total += romans[compound]
-                            curr += 2
-                            continue
-                        else:
-                            return 0
-                            # total = romans[lttr] - total
-                            # curr += 1
+                        total += romans[lttr]
+                        total -= (2 * romans[roman_string[curr - 1]])
+                        curr += 1
                     elif romans[lttr] == romans[roman_string[curr - 1]]:
                         total = romans[lttr] + total
                         curr += 1
