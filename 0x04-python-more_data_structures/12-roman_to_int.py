5029ac0709d9ee853868855b6e8571f7ed7cc43d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-19 20:39:42 -0500

    fix: pep format

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,6 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
-    #if len(roman_string) == 0:
-        #return 0
 
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     curr = 0
