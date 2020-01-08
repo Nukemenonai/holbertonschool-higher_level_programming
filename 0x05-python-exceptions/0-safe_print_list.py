41a3733de686d6948457157cf7fc0efdd5082500
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-08 14:50:47 -0500

    fix: corrected return counter

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     c = 0
     try:
-        for i in range(x):
+        for i in range(0, x):
             print("{}".format(my_list[i]), end="")
             c += 1
     except IndexError:
         pass
     finally:
         print('')
-        return (i)
+        return (c)
