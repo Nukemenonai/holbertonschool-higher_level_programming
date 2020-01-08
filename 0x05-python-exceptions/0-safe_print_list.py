31e7281faacb5b9e3032c75b40f20e2ea3127286
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-08 12:04:15 -0500

    [ADD] task 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for item in my_list[:x]:
+        try:
+            if item is not my_list[-1] and x - 1 > i:
+                print("{}".format(item), end="")
+            else:
+                print("{}".format(item), end='\n')
+            i += 1
+        except IndexError:
+            return (i)
+    return (i)
+
+#    while x:
+#       try:
+#            print("{}".format(my_list[i]), end="")
+#            #print("" if x > 1 else "\n")
+#            i += 1
+#            x -= 1
+#        except (IndexError):
+#            return (i)
+#    return (i)
