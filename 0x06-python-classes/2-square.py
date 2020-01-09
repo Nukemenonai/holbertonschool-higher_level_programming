907657e446c3a11167a68c72092df1199155cf00
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-09 16:50:56 +0000

    [ADD] square with size validation with exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """ init method for square class
+
+        Args:
+            Size: the size of the square
+        """
+        try:
+            if size < 0:
+                raise ValueError
+            elif type(size) != int:
+                raise TypeError
+            else:
+                self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")
