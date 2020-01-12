54842f0e582211af6d9d91efba02fb9c1b120cb2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-12 01:39:37 +0000

    [fix]: number of required docstring lines

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+TEST add module
+what is this
+return: sum 
+"""
+
 def add_integer(a, b=98):
-    """ Adds 2 integers """
+    """
+        Adds 2 integers
+    """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
