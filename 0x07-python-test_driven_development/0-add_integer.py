4a668de1622c4c5a0167247423118082db9b8c31
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-12 02:01:31 +0000

    [fix]: conversion of floats to ints

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,19 +2,19 @@
 """
 TEST add module
 what is this
-return: sum 
+return: sum
 """
 
 def add_integer(a, b=98):
     """
         Adds 2 integers
     """
-    if type(a) != int and type(a) != float:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     if type(a) == float:
-        int(a)
+        a = int(a)
     if type(b) == float:
-        int(b)
+        b = int(b)
     return a + b
