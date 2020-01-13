99a40281a2e0d31951e031a975aaf242b7928d02
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-13 15:18:18 -0500

    fixed tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -28,4 +28,24 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_exceed_param_number(self):
         with self.assertRaises(TypeError):
-            self.assertEqual(max_integer(2,4), 4)
+            self.assertEqual(max_integer(2, 4), 4)
+
+    def test_single_tuple(self):
+        self.assertEqual(max_integer([(2, 4)]), (2, 4))
+
+    def test_dual_tuple(self):
+        self.assertEqual(max_integer([(1, 3), (4, 5)]), (4, 5))
+
+    def test_true(self):
+        self.assertEqual(max_integer([1,2,3,5,True]), 5)
+
+    def test_complex(self):
+        with self.assertRaises(TypeError):
+            max_integer([34, 78, 12, 5j])
+
+    def test_hexa(self):
+        self.assertEqual(max_integer([12, 34, 56, 0xFFFFFF]), 16777215)
+        self.assertEqual(max_integer([1234, 345, 0xFEDCBA]), 16702650)
+
+    def test_function_inside(self):
+        self.assertEqual(max_integer([max_integer([1, 2]), max_integer([3, 4])]), 4)
