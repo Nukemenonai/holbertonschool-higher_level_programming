7370f3bc3eb53c6ccadb5b21d9d1beb58c511531
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-13 13:51:42 -0500

    added more tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,9 +4,28 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     def test_Integer(self):
-        self.assertAlmostEqual(max_integer([1,2,3,4]), 4)
-        self.assertAlmostEqual(max_integer([]), None)
-        self.assertAlmostEqual(max_integer([4,4,4,4]), 4)
-        self.assertAlmostEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_voidlist(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_equal_elements(self):
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+        self.assertEqual(max_integer([-4, -4, -4, -4]), -4)
+
+    def test_matrix_zero(self):
+        self.assertEqual(max_integer([0]), 0)
+
+    def test_letters(self):
+        self.assertEqual(max_integer(['a', 'b', 'c', 'd']), 'd')
+
+    def test_hybrid_list(self):
+        with self.assertRaises(TypeError):
+            self.assertEqual(max_integer(['a', 'b', 4, 2]), 'b')
+
+    def test_exceed_param_number(self):
+        with self.assertRaises(TypeError):
+            self.assertEqual(max_integer(2,4), 4)
