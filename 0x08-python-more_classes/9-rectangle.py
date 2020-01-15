716cd3e229c99210ffcabe5d4880510b1c3aec6e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-15 16:41:46 -0500

    fix: class method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,18 +10,27 @@ class Rectangle:
            width:
            height:
         """
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if type(width) != int:
+        self.width = width
+        self.height = height
+        self.print_symbol = Rectangle.print_symbol
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """ width property of the rectangle """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """Init method for rectangle class
+        Args:
+        width: width of the rectangle
+        """
+        if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.__width = width
-        self.__height = height
-        self.print_symbol = Rectangle.print_symbol
-        Rectangle.number_of_instances += 1
 
     @property
     def height(self):
@@ -37,23 +46,6 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
-
-    @property
-    def width(self):
-        """ width property of the rectangle """
-        return self.__width
-
-    @width.setter
-    def width(self, width):
-        """Init method for rectangle class
-        Args:
-        width: width of the rectangle
-        """
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
 
     def area(self):
         """defines the area of my rectangle"""
@@ -103,6 +95,5 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        cls.width = size
-        cls.height = size
-        return cls(cls.width, cls.height)
+        """ the info given is sent to parent class for verification"""
+        return Rectangle(size, size)
