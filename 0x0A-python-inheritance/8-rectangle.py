7c8c0a2371a33a28073cc02bc5fd374fcb609e6b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-20 18:41:13 +0000

    fix: importing instead of copying class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 
 
-class BaseGeometry:
-    """ area method not implemented """
-
-    def area(self):
-        """ method not implemented """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates value """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        "string representation of a rectangle"
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __repr__(self):
+        """ represents a rectangle """
+        return "[Rectangle]{}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """ """
+        return self.__width * self.__height
