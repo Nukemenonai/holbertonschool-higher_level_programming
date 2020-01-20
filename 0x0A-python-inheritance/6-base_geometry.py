2d406cfd2e6556fc51beff857908041ec5a05d4b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-20 16:06:51 +0000

    add: file for task 6 area method not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """ area method not implemented """
+
+    def area(self):
+        """ method not implemented """
+        raise Exception("area() is not implemented")
