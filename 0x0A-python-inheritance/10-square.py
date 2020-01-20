f6cf50a94b0f4363cf8e4e818604b4641240dc9d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-20 19:23:52 +0000

    fix: making size private

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,4 +10,5 @@ class Square(Rectangle):
     def __init__(self, size):
         """ istantiates a square """
         super().integer_validator("size", size)
-        super().__init__(size, size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
