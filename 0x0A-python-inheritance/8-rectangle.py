f7bad6d43c1c4999f898e7b872b457daeac5888c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-20 17:45:21 +0000

    task 8 rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """ area method not implemented """
+
+    def area(self):
+        """ method not implemented """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates value """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
