170547f3ab00d54445bc06acfc56383ab4e8c61d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-20 15:16:04 +0000

    fix: docstring and pep8

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+
 class MyList(list):
     """ Class that inherits from list """
 
     def print_sorted(self):
+        """prints a sorted copy of the instance MyList"""
         new_list = self.copy()
         new_list.sort()
         print(new_list)
