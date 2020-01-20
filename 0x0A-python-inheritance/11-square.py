b03191da3ffab362f4e1acf7a31c5ea9dd4360c0
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-20 19:21:53 +0000

    add: square str representation

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """inherits from rectangle class"""
+
+    def __init__(self, size):
+        """ istantiates a square """
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        "string representation of a square"
+        return "[Square] {}/{}".format(self.__size, self.__size)
