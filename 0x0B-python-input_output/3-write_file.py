c563447e238a34858ed3bb017b8af4b569ef5a39
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-21 14:46:00 +0000

    [feature]: write string to a text file, return # of chars written

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """ writes a string UTF8 to a text file
+    returns the number of characters written
+    filename: name of the file.
+    text: the text to insert
+    """
+
+    with open(filename, 'w') as f:
+        n = f.write(text)
+        return n
