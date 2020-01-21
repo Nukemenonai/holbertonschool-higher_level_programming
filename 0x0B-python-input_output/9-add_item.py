aeb35f4177a60ce555ab48cade9b37045321cda9
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-21 19:11:08 +0000

    feature: add all args to a python list n save 'em to json file

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" script that adds all arguments to a python list """
+
+import sys
+import json
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+f = open("add_item.json", 'a')
+f.close()
+if len(open("add_item.json").readlines()) == 1:
+    my_list = load_from_json_file("add_item.json")
+else:
+    my_list = []
+    save_to_json_file(my_list, "add_item.json")
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+    save_to_json_file(my_list, "add_item.json")
