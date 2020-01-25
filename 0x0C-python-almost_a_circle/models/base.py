417bfd25485026b7209df260a2580b5d134e32f5
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-25 01:14:04 +0000

    [feature] json dictionary representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import json
+
 
 class Base:
     """ Base class """
@@ -13,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns JSON representation of a list of dict """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
