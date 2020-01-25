4df478052cf7f98ab3d398366ba06b6f95cb80ea
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-25 17:06:21 +0000

    added classmethod to save to json file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,6 @@
 
 import json
 
-
 class Base:
     """ Base class """
 
@@ -16,9 +15,25 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON representation of a list of dict """
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON representation of list_objs to a file"""
+        if list_objs is None:
+            with open("{}.json".format(cls.__name__), 'w') as f:
+                f.write("[]")
+                f.close()
+        else:
+            json_list = []
+            with open("{}.json".format(cls.__name__), 'w') as f:
+                for item in list_objs:
+                    json_list.append(item.to_dictionary())
+                f.write(str(json_list))
+                f.close()
