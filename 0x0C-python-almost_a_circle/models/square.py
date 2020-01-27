8809d63402e37ed1c85735cb4dd2aea2c8f7b5cd
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-27 14:33:23 -0500

    fix: len 0 in args

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,7 +34,7 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """ assigns an argument to each attribute"""
-        if args and args is not None:
+        if args is not None and len(args) != 0:
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
