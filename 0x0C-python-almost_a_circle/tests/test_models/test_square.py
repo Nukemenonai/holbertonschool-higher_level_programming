9676249564c9b8d1b24f1ed4795fccf15bfb93a2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-27 17:45:23 -0500

    more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -33,3 +33,17 @@ class Test_id(unittest.TestCase):
             s.x = {}
         with self.assertRaises(ValueError):
             Square(10, 3, -1)
+
+    def test_sq_area(self):
+        s1 = Square(3)
+        self.assertEqual(s1.area(), 9)
+        s2 = Square(10)
+        self.assertEqual(s2.area(), 100)
+        s3 = Square(8, 0, 0, 12)
+        self.assertEqual(s3.area(), 64)
+
+    def test_sq_str_(self):
+        s1 = Square(4, 2, 1, 12)
+        self.assertEqual(s1.__str__(), "[Square] (12) 2/1 - 4")
+        s2 = Square(5, 1)
+        self.assertEqual(s2.__str__(), "[Square] (1) 1/0 - 5")
