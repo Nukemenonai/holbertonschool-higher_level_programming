92901df1a180e8d81b338f090b6cc357ddeda4ef
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-27 03:18:49 +0000

    .

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,26 @@
+""" test file bor Rectangle class """
+
+import unittest
+
+from models.base import Rectangle
+
+class Test_id(unittest.TestCase):
+
+    """ """
+    def test_init(self):
+        """ test init """
+        b1 = Base()
+        self.assertEqual(print(b1.id), 1)
+
+        b2 = Base()
+        self.assertEqual(print(b2.id), 2)
+
+        b3 = Base()
+        self.assertEqual(print(b3.id), 3)
+
+        b4 = Base(12)
+        self.assertEqual(print(b4.id), 12)
+
+        b5 = Base()
+        self.assertEqual(print(b5.id), 4)
+
