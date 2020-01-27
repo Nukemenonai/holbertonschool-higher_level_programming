680cc11e990b133f052387ced0c136aaa2cf39c9
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-27 04:09:09 +0000

    progress

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -16,15 +16,15 @@ class Test_id(unittest.TestCase):
         """ test init """
         Base.__nb_objects = 0
         b1 = Base()
-        self.assertEqual(print(b1.id), 1)
+        self.assertEqual(b1.id, 1)
         b2 = Base()
-        self.assertEqual(print(b2.id), 2)
+        self.assertEqual(b2.id, 2)
         b3 = Base()
-        self.assertEqual(print(b3.id), 3)
+        self.assertEqual(b3.id, 3)
         b4 = Base(12)
-        self.assertEqual(print(b4.id), 12)
+        self.assertEqual(b4.id, 12)
         b5 = Base()
-        self.assertEqual(print(b5.id), 4)
+        self.assertEqual(b5.id, 4)
 
 if __name__ == '__main__':
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,17 +10,16 @@ class Test_id(unittest.TestCase):
 
     """ """
     def setUp(self):
-        Base.__nb_objects = 0
+        Base._Base__nb_objects = 0
 
     def test_id(self):
         """ test id of a rectangle """
-        Base.__nb_objects = 0
         r1 = Rectangle(10, 2)
-        self.assertEqual(print(r1.id), 1)
+        self.assertEqual(r1.id, 1)
         r2 = Rectangle(2, 10)
-        self.assertEqual(print(r2.id), 2)
+        self.assertEqual(r2.id, 2)
         r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(print(r3.id), 12)
+        self.assertEqual(r3.id, 12)
 
     def test_attributes(self):
         """ """
@@ -34,6 +33,14 @@ class Test_id(unittest.TestCase):
             r.x = {}
         with self.assertRaises(ValueError):
             Rectangle(10, 2, 3, -1)
+
+    def test_area(self):
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
 
 
 if __name__ == '__main__':
