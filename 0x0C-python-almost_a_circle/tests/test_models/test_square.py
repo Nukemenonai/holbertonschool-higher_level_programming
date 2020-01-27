b12c5bdbdf7a939e24cb1103cdb693623dce298a
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-27 18:03:21 -0500

    add: more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -60,3 +60,22 @@ class Test_id(unittest.TestCase):
         self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 10")
         with self.assertRaises(TypeError):
             s1.size = "9"
+
+    def test_sq_update(self):
+        """ """
+        s1 = Square(5)
+        self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 5")
+        s1.update(10)
+        self.assertEqual(s1.__str__(), "[Square] (10) 0/0 - 5")
+        s1.update(1, 2)
+        self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 2")
+        s1.update(1, 2, 3)
+        self.assertEqual(s1.__str__(), "[Square] (1) 3/0 - 2")
+        s1.update(1, 2, 3, 4)
+        self.assertEqual(s1.__str__(), "[Square] (1) 3/4 - 2")
+        s1.update(x=12)
+        self.assertEqual(s1.__str__(), "[Square] (1) 12/4 - 2")
+        s1.update(size=7, y=1)
+        self.assertEqual(s1.__str__(), "[Square] (1) 12/1 - 7")
+        s1.update(size=7, id=89, y=1)
+        self.assertEqual(s1.__str__(), "[Square] (89) 12/1 - 7")
