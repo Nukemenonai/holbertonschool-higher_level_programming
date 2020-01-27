df84b81443870cba07cb8e5adadd8c74f7bcdbbf
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-27 14:27:19 -0500

    fix: private attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,9 +40,9 @@ class Square(Rectangle):
             if len(args) >= 2:
                 self.width = args[1]
             if len(args) >= 3:
-                self.__x = args[2]
+                self.x = args[2]
             if len(args) >= 4:
-                self.__y = args[3]
+                self.y = args[3]
         else:
             if 'id' in kwargs:
                 self.id = kwargs['id']
