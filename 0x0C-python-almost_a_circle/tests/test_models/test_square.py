191117bd6f9754ebe5b54ed960a5ed9ba2c1e82b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-27 03:20:58 +0000

    .

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,11 @@
+""" test file bor base class """
+
+import unittest
+
+from models.base import Base
+
+class Test_id(unittest.TestCase):
+
+    """ """
+    def test_init(self):
+        """ test init """
