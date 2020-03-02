440b9b5eae6c438c7542e38ded60f8ffdf042d26
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-02 15:17:24 -0500

    fix: order of the queries

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists number of records with same score
-SELECT score FROM second_table GROUP BY COUNT(score) > 1 AS number;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
