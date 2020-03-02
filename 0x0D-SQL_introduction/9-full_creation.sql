ea0ee5d4c302da5fa2c7e59a6a6b036c1748f7ff
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-02 14:33:52 -0500

    fix: order of first query

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 --
-CREATE TABLE second_table IF NOT EXISTS (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES
 	(1, "John", 10),
