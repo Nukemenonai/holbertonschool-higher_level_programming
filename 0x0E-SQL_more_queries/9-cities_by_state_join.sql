b9f94b0c5b5c87e29bb83e92c3b7740353bf54b6
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 17:13:31 -0500

    fix: what table to look from

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in database
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
