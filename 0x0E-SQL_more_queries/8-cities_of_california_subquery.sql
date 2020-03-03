89e39455076efff495f18f6dd48f6adb6df65898
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 16:50:22 -0500

    fix order of the subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- Lists all cities of california that can be found in database
-SELECT * FROM cities WHERE (
-       SELECT *
-       FROM states
-       WHERE name = 'California'
-) ORDER BY cities.id ASC;
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE state_id = (states.id=cities.state_id AND states.name = 'California')
+ORDER BY cities.id ASC;
