51021094a6be04cfb2208d119dde8941d34a6690
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 17:10:26 -0500

    fix: join table

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- lists all cities contained in database
 SELECT cities.id, cities.name, states.name
-FROM cities, states
+FROM cities
+JOIN states ON cities.id = states.id
 ORDER BY cities.id ASC;
