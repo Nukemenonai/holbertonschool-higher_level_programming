aa1fd68b5b4e96d69215184aee528e4e96bb7750
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 14:44:34 -0500

    fix: not null field

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- creates database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-       id INT UNIQUE AUTO_INCREMENT,
+       id INT NOT NULL UNIQUE AUTO_INCREMENT,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY (id)
 );
