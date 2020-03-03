2bc6e4634abecfeee4eecc1e50aa046d92e198f2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 12:46:44 -0500

    fix: comment line

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- List all privileges of an user
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+-- List all privileges of an user
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
