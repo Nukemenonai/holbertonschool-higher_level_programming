092b67060a61ef6a6e73e3cb5b26c5ad5d770faa
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 14:54:02 -0500

    fix comment 0?

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- creates database hbtn_0d_usa
+-- creates table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
        id INT UNIQUE AUTO_INCREMENT NOT NULL,
