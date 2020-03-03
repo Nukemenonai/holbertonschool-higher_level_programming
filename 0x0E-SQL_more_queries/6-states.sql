92bfb5a49c933749bc11bcd0d9c734e730537e1b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 14:41:50 -0500

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+       id INT UNIQUE AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id)
+);
