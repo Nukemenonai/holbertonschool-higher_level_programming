4ed00540b7a54189c188933f913e40a2b06d8822
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 14:57:53 -0500

    fix: specified database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- creates database hbtn_0d_usa
 -- creates table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT NOT NULL UNIQUE AUTO_INCREMENT,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY (id)
 );
