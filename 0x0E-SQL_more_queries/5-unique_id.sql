b692f8e2e8d44019ce601870887c4f2d189a2ce5
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-03 14:20:15 -0500

    fixed comments

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,5 @@
- -- creates the table unique_id
+-- creates the table unique_id
+-- see if this solves it
 CREATE TABLE IF NOT EXISTS unique_id (
        id INT DEFAULT 1 UNIQUE,
        name VARCHAR(256)
