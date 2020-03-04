e2d2e5735fbe2a87998b8307b73c4d58fa8df2ba
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-04 01:46:54 -0500

    fix: whatever

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- lists all shows without the genre commedy
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN(
+	SELECT tv_shows.title as shows
+	FROM tv_shows
+	JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy'
+) AS comedy_shows
+ON tv_shows.title = comedy_shows.shows
+WHERE comedy_shows.shows IS NULL
+ORDER BY tv_shows.title ASC;
