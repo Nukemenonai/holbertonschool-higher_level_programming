e27144d8cc79e95a210a4dd7ebf292ac0b8644de
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-28 15:31:56 +0000

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' first script using sqlalchemy'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__= 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
