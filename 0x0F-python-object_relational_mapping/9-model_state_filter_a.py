c8c8d99dad8026c19f0b6ce02f3ecec51c92c48a
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-28 17:31:50 +0000

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script lists all state object from database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.sql import select
+from sqlalchemy import (create_engine)
+
+if __name__ == '__main__':
+    a = sys.argv[1]
+    b = sys.argv[2]
+    c = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(a,
+                                                                       b,
+                                                                       c),
+                           pool_pre_ping=True)
+    conn = engine.connect()
+    sql = select([State]).where(State.name.like('%a%'))
+    res = conn.execute(sql)
+
+    for row in res:
+        print("{}: {}".format(row[0], row[1]))
