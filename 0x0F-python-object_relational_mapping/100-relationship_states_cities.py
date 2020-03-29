e7611e5744f9a1484c14ac88b7e67fc26ae5e926
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-29 16:26:13 +0000

    update: code to add city and state

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -4,8 +4,8 @@ City Relationship
 """
 
 import sys
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
