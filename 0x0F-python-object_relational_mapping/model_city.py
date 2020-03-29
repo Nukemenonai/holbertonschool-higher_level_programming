a4f01d57ad0e2fbb0ddbc2bd47a2d0e42109c59d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-29 01:47:35 +0000

    fix: attibutes for city id

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 ''' first script using model for cities'''
 
-
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
 
 
 class City(Base):
         """ Class for table city """
         __tablename__ = 'cities'
-        id = Column(Integer, primary_key=True, nullable=False)
+        id = Column(Integer, unique=True, primary_key=True, nullable=False)
         name = Column(String(128), nullable=False)
         state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
