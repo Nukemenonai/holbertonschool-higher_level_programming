ef95b0326f6131857661d0d52541326d28024312
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-29 01:24:28 +0000

    task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This script lists all city objects from database hbtn_0e_14_usa"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    a = sys.argv[1]
+    b = sys.argv[2]
+    c = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(a,
+                                                                       b,
+                                                                       c),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    sq = session.query(State.name, City.id, City.name).\
+            join(City, State.id == City.state_id).\
+            order_by(City.id)
+    res = sq.all()
+
+    for row in res:
+        print("{:s}: ({:d}) {:s}".format(*row))
