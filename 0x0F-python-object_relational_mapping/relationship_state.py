95c7c6494556e290d7b825f98c0c89c8315f5575
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-29 02:56:16 +0000

    add relationships

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''model for states'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+        """ Class for table state  """
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(128), nullable=False)
+        cities = relationship('City')
