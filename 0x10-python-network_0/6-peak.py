4a215b53208d96d7d0269b38d85f4dc06cc8793a
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-06 21:56:45 +0000

    task 6: find peak element

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+findpeak module
+"""
+
+def find_peak(list_of_integers):
+    """ function to find the peak element in a list """
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    else:
+        peak = list_of_integers[0]
+        for item in list_of_integers:
+            if item > peak:
+                peak = item
+        return peak
