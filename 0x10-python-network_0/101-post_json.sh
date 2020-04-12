497edcb9df69706d200d4113c265190172bb6382
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-12 22:41:07 +0000

    test #2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script returs status code only
-curl -sL -X POST -H "Content-Type: application/json" -d "$2" "$1"
+curl -sX POST -H "Content-Type: application/json" -d $(cat "$2") "$1"
