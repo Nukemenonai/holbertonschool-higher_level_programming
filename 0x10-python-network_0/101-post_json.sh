1bb2b9c5fa33d51b4caa459a65f5105a7928e735
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-12 22:59:11 +0000

    fix : used @

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script returs status code only
-curl -X POST -k -i -H "Accept: application/json" -H "Content-Type: application/json" -w "%{body}" -w "%{http_code}" -d "$2" "$1"
+curl -X POST -H "Content-Type: application/json"  -d @"$2" "$1"
