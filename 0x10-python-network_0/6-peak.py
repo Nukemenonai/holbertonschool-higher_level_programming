f540cd20fc392e946da070ef25575b4070d8807c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-12 21:46:12 +0000

    fix: used binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,9 +3,36 @@
 findpeak module
 """
 
+def peak_alg(arr, low, high, n):
+    """ algorithm"""
+
+    mid = (low + high) /2
+    mid = int(mid)
+
+    if ((mid == 0 or arr[mid - 1] <= arr[mid]) and
+       (mid == n - 1 or arr[mid + 1] <= arr[mid])):
+        return arr[mid]
+
+    elif (mid > 0 and arr[mid + 1] > arr[mid]):
+        return peak_alg(arr, (mid + 1), high, n)
+
+    else:
+        return peak_alg(arr, low, (mid - 1), n)
+
+
 
 def find_peak(list_of_integers):
-    """ function to find the peak element in a list """
+    """ finds a peak in an array """
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    n = len(list_of_integers)
+    res = peak_alg(list_of_integers, 0, n - 1 , n)
+    return res
+
+
+"""
+def find_peak(list_of_integers):
+    function to find the peak element in a list
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
     else:
@@ -14,3 +41,4 @@ def find_peak(list_of_integers):
             if item > peak:
                 peak = item
         return peak
+"""
