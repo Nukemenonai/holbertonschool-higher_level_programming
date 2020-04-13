9bd2cf803e6a957ab231cccaed2a679184e90bca
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-13 16:30:35 +0000

    fix something

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script does sometinhg
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d user_id=98 -H "Origin: HolbertonSchool"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin:HolbertonSchool"
