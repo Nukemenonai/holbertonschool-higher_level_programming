287f4147cef8ff014e20c6962f8a464dc01eb664
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-13 02:22:39 +0000

    what do you want from me?

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script does sometinhg
-curl -sL 0.0.0.0:5000/catch_me -X PUT -d user_id=98 -H "Origin: HolbertonSchool"
+curl -sL -X PUT -d user_id=98 -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me 
