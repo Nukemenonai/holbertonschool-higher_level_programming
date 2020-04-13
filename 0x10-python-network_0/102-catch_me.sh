d5fa9378a4aced26b2c9d716b9fc6966e8ba8e9b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-13 02:19:31 +0000

    specified user id without quotes

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# this script uploads JSON
-curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
+# this script does sometinhg
+curl -sL 0.0.0.0:5000/catch_me -X PUT -d user_id=98 -H "Origin: HolbertonSchool"
