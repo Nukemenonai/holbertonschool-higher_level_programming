3d5392ca3b844f717f6e4977b2558dd44cedb92e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-14 22:32:17 +0000

    [feature] script that fetches body response of an URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this script fetches an URL """
+
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as resp:
+        data = resp.read()
+        html = data.decode("UTF-8")
+
+        print ("Body Response:")
+        print ("    - type: {}".format(type(data)))
+        print ("    - content: {}".format(data))
+        print ("    - utf8 content: {}".format(html))
