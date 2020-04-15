f733c427a292cd7fe8f4ebee699ece95b8b5a591
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-15 22:14:20 +0000

    identation level

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,8 +8,8 @@ import sys
 
 
 if __name__ == "__main__":
-        try:
-                with request.urlopen("{}".format(sys.argv[1])) as resp:
-                        print(resp.read().decode('UTF-8'))
-        except urllib.error.HTTPError as e:
-                print("Error code: {}".format(e.code))
+    try:
+        with request.urlopen("{}".format(sys.argv[1])) as resp:
+            print(resp.read().decode('UTF-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
