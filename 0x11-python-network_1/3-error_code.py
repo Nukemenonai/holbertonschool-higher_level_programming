ca721649f3460fac903b61c2c4785fb87c11c8f2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-15 22:05:56 +0000

    task 3 getting error codes

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""this script fetches an URL """
+
+from urllib import request, parse
+import urllib
+import sys
+
+
+if __name__ == "__main__":
+        try:
+            with request.urlopen("{}".format(sys.argv[1])) as resp:
+                print(resp.read().decode('UTF-8'))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
