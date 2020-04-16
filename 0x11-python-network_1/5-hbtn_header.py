0bad8239b97d6514ea0052e6c4179dbdeea7d183
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-16 00:27:57 +0000

    task 5: getting a header value

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""this script fetches"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(''.format(sys.argv[1]))
+    print(r.headers.get('X-Request-Id'))
