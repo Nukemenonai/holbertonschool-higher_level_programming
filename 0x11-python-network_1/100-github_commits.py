ad14674600d8457c85f7e80132c60acfc3e48e38
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-17 22:06:20 +0000

    consulting the twitter api with python

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""this script fetches something """
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[1],
+                             sys.argv[2]))
+    data = r.json()
+    for i in range(10):
+        print("{}: {}".format(data[i]['sha'], data[i]['committer']['login']))
