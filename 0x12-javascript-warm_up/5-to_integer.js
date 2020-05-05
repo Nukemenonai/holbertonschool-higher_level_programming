b41cc3f36567bded7cac9e731e26338cb4feb3e5
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-05 17:39:28 +0000

    fix: checked for NaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 if (process.argv[2]) {
-  if (typeof (parseInt(process.argv[2])) === 'number') {
+  if (typeof (parseInt(process.argv[2]), 10) === 'number' &&
+      isNaN(parseInt(process.argv[2])) === false) {
     console.log('My number: ' + parseInt(process.argv[2]));
   } else {
     console.log('Not a number');
