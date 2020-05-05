4509fedc1a5a92990e9f89a9a27e0b7646ce6130
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-05 17:26:51 +0000

    fix: additional else

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,6 +3,8 @@
 if (process.argv[2]) {
   if (typeof (parseInt(process.argv[2])) === 'number') {
     console.log('My number: ' + parseInt(process.argv[2]));
+  } else {
+    console.log('Not a number');
   }
 } else {
   console.log('Not a number');
