7534905cda31f1b3c6e2d990eb7c114eeca15f24
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-05 18:49:15 +0000

    [fix] checking for NaN

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv[2] && isNaN(process.argv[2] === false)) {
+if (process.argv[2] && !isNaN(parseInt(process.argv[2]))) {
   for (let i = 0; i < process.argv[2]; i++) {
     console.log('X'.repeat(process.argv[2]));
   }
