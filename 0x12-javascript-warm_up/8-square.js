01fa32482a4c6537fe7ac77d2f8ae59899604f58
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-05 18:10:41 +0000

    [task 8] printing a square with loops

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv[2]) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('x'.repeat(process.argv[2]));
+  }
+} else {
+  console.log('Missing size');
+}
