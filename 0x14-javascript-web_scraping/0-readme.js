24afe74f8e6bbf4753923b4e86243c13f6043f8e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-12 14:11:40 -0500

    [update]: turned console.log a return

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    return console.log(err);
   }
-  console.log(data);
+  process.stdout.write(data);
 });
